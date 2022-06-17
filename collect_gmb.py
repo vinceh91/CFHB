2d6558a869b8b750719d3ee1692cf4b4fb370a01
Vincent Haguet <vincent.haguet@total.com>
2022-06-17 18:19:09 +0200

    Refactor etl #1

--- collect_gmb.py
+++ collect_gmb.py
@@ -9,7 +9,31 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import googleapiclient
 from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, VisualStudioCodeCredential
+import config
+from gmb_functions import get_locations, preprocess_reviews, write_to_blob
+
+import logging
+import sys
+
+# ...
+import os
+
+from gmb_functions import get_accounts_dataframe
+# os.environ["HTTP_PROXY"] = "http://10.10.1.10:1180"
+
+# Alternate URL and variable forms:
+# os.environ["HTTP_PROXY"] = "http://username:password@10.10.1.10:1180"
+# os.environ["HTTPS_PROXY"] = "http://10.10.1.10:1180"
+# os.environ["HTTPS_PROXY"] = "http://username:password@10.10.1.10:1180"
+# Acquire the logger for a library (azure.mgmt.resource in this example)
+# logger = logging.getLogger('azure')
+
+# Set the desired logging level
+# logger.setLevel(logging.DEBUG)
+
+# handler = logging.StreamHandler(stream=sys.stdout)
+# logger.addHandler(handler)
 
 ### LUIS
 luis_apps = {'fr' : '0a5be55e-0c32-482f-abcc-287cb9952f40',
@@ -17,12 +41,13 @@ luis_apps = {'fr' : '0a5be55e-0c32-482f-abcc-287cb9952f40',
              'nl' : 'f176f20b-a4fa-470c-af05-ea4e72343412' }
 
 ## Param spécifiques
-account_list = ['accounts/102280495497198834033', 'accounts/102543083042626102334', 'accounts/116116273817792355878']  # BE + NL + LU
+accounts_list = ['accounts/102280495497198834033', 'accounts/102543083042626102334', 'accounts/116116273817792355878']  # BE + NL + LU
 #account_list = ['accounts/108458503997377848869'] #KE
+
 
 nb_jours_reprise = 1
 get_accounts = True
-get_locations = True
+# get_locations = True
 get_reviews = True
 cfh_scope = "cfhb"
 analysis_language =  "en" #"en-us"
@@ -46,29 +71,72 @@ date_ref = str(datetime.datetime.now()-datetime.timedelta(days=nb_jours_reprise)
 container_param = 'customervoice-param'
 container_stock = 'data'
 container_gmb_ref = 'gmb-data-ref'
-fl_fbacks = "feedbacks/fbacks_"+date+".csv"
-fl_original = "original/data_"+date+".csv"
-fl_luis = "luis/luis_"+date+".csv"
+fl_fbacks = "feedbacks/fbacks_"+ date +".csv"
+fl_original = "original/data_"+ date +".csv"
+fl_luis = "luis/luis_"+ date +".csv"
 fl_phrases = "keyphrases/phrases_"+date+".csv"
 
 ########################################################################################
 #Getting secrets
 
 #Storage accounts
-# blob_param = dbutils.secrets.get(scope = cfh_scope, key = "blob-param-string")
-# blob_stock = dbutils.secrets.get(scope = cfh_scope, key = "blob-stock-string")
-keyVaultName = "azkvpcfhb02" 
-KVUri = f"https://{keyVaultName}.vault.azure.net"
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
-secretName = "blob-param-string"
-retrieved_secret = client.get_secret(secretName)
+blob_param = config.BLOB_PARAM_STRING
+blob_stock = config.BLOB_STOCK_KEY
+# keyVaultName = "azkvpcfhb02" 
+# KVUri = f"https://{keyVaultName}.vault.azure.net"
+# credential = DefaultAzureCredential(ogging_enable=True)
+# # credential = VisualStudioCodeCredential(logging_enable=True)
+# client = SecretClient(vault_url=KVUri, credential=credential, logging_enable=True, proxies={ "https": "https://AJ0545269@admin.hubtotal.net:NDjokovic91!@10.10.1.10:1180"})
+# secretName = "blob-param-string"
+# retrieved_secret = client.get_secret(secretName).value
+
 # #Services cognitifs
-# Key_Text_Translation = dbutils.secrets.get(scope = cfh_scope, key = "tt-key")
-# Key_Text_Analysis = dbutils.secrets.get(scope = cfh_scope, key = "ta-key")
-# Key_LUIS = dbutils.secrets.get(scope = cfh_scope, key = "lu-key")
-# #SQL DB
+key_text_translation = config.TT_KEY
+key_text_analysis = config.TA_KEY
+key_luis = config.LU_KEY
 
 # # mise en forme
-# connect_param = {'string' : blob_param,
-#                  'container' : container_param }
+connect_param = {'string' : blob_param,
+                 'container' : container_param }
+
+blobparam_service_client = BlobServiceClient.from_connection_string(blob_param)
+
+blob_client = blobparam_service_client.get_blob_client(container=container_param,
+                                                                  blob=gmb_token_file)        
+creds = pickle.loads(blob_client.download_blob().readall())
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+        print("Credentials Refresh : OK")
+    elif creds.valid:
+        creds.refresh(Request())
+        print("Credentials Refresh : OK")
+
+accounts_service = build('mybusinessaccountmanagement', 'v1', credentials=creds, discoveryServiceUrl = discovery_uri_manag, static_discovery=False)
+df_acc = get_accounts_dataframe(accounts_service)
+
+locations_service = build('mybusinessbusinessinformation', 'v1', credentials=creds, discoveryServiceUrl = discovery_uri_info, static_discovery=False)
+df_locations = get_locations(locations_service, accounts_list)
+
+blob_client = blobparam_service_client.get_blob_client(container='customervoice-param',
+                                                  blob="station_list.csv")
+my_string = str(blob_client.download_blob().readall() ,'latin-1')
+data = StringIO(my_string) 
+df_stations = pd.read_csv(data, sep=";")
+df_stations = df_stations[['Store ID', 'Country', 'Country name', 'REGION', 'MANAGEMENT MODE', 'Location Name']]
+df_stations = df_stations.merge(df_locations[['storeCode','name']],
+                                        left_on='Store ID',
+                                        right_on='storeCode',
+                                        how='right')
+
+reviews_service = build('mybusiness', 'v4', credentials=creds, discoveryServiceUrl = discovery_uri, static_discovery=False)
+df_reviews = get_reviews(reviews_service, df_locations, date_ref)
+
+#### Write to blob
+write_to_blob(blob_stock, container_gmb_ref, "location_accounts.csv", df_acc)
+write_to_blob(blob_stock, container_gmb_ref, "station_raw.csv", df_locations)
+write_to_blob(blob_stock, container_gmb_ref, "station_enrich.csv", df_stations)
+write_to_blob(blob_stock, container_stock, fl_original, df_reviews)
+
+df_reviews_preprocessed = preprocess_reviews(df_reviews, df_stations)
+
---
+++ gmb_functions.py
@@ -0,0 +1,151 @@
+import datetime
+import pandas as pd
+import googleapiclient
+from azure.storage.blob import BlobServiceClient
+from io import BytesIO
+import numpy as np
+
+def time_printer_ext():
+  return(datetime.datetime.now())
+
+def rating_transco(ma_string):
+    #Revu de la notation stars de google vers des entiers
+    if ma_string == 'FIVE':
+        return 5
+    elif ma_string == 'FOUR':
+        return 4
+    elif ma_string == 'THREE':
+        return 3
+    elif ma_string == 'TWO':
+        return 2
+    elif ma_string == 'ONE':
+        return 1
+    elif ma_string == 'ZERO':
+        return 0
+    else :
+        return np.nan
+
+def extract_username(mon_dict):
+    #formattage de colonne user
+    try:
+        return mon_dict['displayName']
+    except:
+        return 'not available'
+
+def extract_location(ma_string):
+    #formatage de colonne location
+    try:
+        return ma_string.split('reviews')[0][:-1]
+    except:
+        return 'not available'
+      
+def to_datetime(ma_string):
+    #formatage de colonnes date
+    try :
+        return datetime.datetime.strptime(ma_string, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except:
+        return datetime.datetime.strptime(ma_string, '%Y-%m-%dT%H:%M:%SZ')
+
+def ggtranslation_delete(ma_str):
+    #Formatage des traductions déjà fournies
+    try:
+        output = ma_str.split('(Original)')[1][1:]
+        try: 
+            output = output.split('(Translated by Google)')[0][0:]
+        except:
+            output = output
+    except:
+        try:
+            output = ma_str.split('(Translated by Google)')[0][0:]
+        except:
+            output = ma_str
+    return output.replace('\r', '').replace('\n', '')
+
+def linesreturn_delete (ma_str):
+    return ma_str.replace('\r', '').replace('\n', '')
+
+def get_accounts_dataframe(service: googleapiclient.discovery.Resource) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        service (googleapiclient.discovery.Resource): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    params = {}
+    all_accounts = []
+    page_token = None
+    while True:
+        if page_token:
+            params['pageToken'] = page_token
+            current_page = service.accounts().list(**params).execute()
+            page_token = current_page.get('nextPageToken')
+            all_accounts.extend(current_page['accounts'])
+        if not page_token:
+            break
+    df_acc = pd.DataFrame(all_accounts)
+    return df_acc
+
+def get_locations(service: googleapiclient.discovery.Resource, accounts_list: list) -> pd.DataFrame:
+    df_loc = pd.DataFrame()
+    for acc in accounts_list:
+        params = {'parent' : acc, "readMask": "storeCode,name"}    
+        page_token = None
+        while True:
+            if page_token:
+                params['pageToken'] = page_token
+            current_page = service.accounts().locations().list(**params).execute()
+            page_token = current_page.get('nextPageToken')
+            current_locations = pd.DataFrame(current_page["locations"])
+            current_locations["name"] = acc + "/" + current_locations["name"]
+            df_loc = pd.concat([df_loc, current_locations])
+            if not page_token:
+                break
+    return df_loc
+
+def get_reviews(service: googleapiclient.discovery.Resource, df_loc: pd.DataFrame, date_ref: str) -> pd.DataFrame:
+    all_reviews = []
+    for loc in df_loc['name']:
+        params = {"parent": loc}
+        page_token = None
+        while True:
+            if page_token:
+                params['pageToken'] = page_token
+            current_page = service.accounts().locations().reviews().list(**params).execute()
+            page_token = current_page.get('nextPageToken')
+            all_reviews.extend(current_page['reviews'])
+            if (not page_token) | (pd.DataFrame(current_page['reviews']).createTime.min()[:10] < date_ref):
+                break
+    df_reviews = pd.DataFrame(all_reviews)            
+    return df_reviews
+
+def write_to_blob(blob_secret: str, container: str, blob: str, df: pd.DataFrame) -> None:
+    blobstock_service_client = BlobServiceClient.from_connection_string(blob_secret)
+    blob_client = blobstock_service_client.get_blob_client(container=container, blob=blob)
+    blob_client.delete_blob()
+    with BytesIO(df.to_csv(sep="|", index=False).encode('utf-8')) as data:
+            blob_client.upload_blob(data)
+    return None
+
+def preprocess_reviews(df: pd.DataFrame, df_stations: pd.DataFrame) -> pd.DataFrame:
+    df['RESPONSERECOMMANDATION'] = df["starRating"].apply(rating_transco)    
+    df['USERNAME'] = "not GDPR compliant"
+    df['location'] = df["name"].apply(extract_location) 
+    df['SURVEYDATE'] = df["createTime"].apply(to_datetime) 
+    df['REFUELDATE'] = np.nan
+    df['USERID'] = 'not available'
+    df['SOURCE'] = 'Google Reviews'    
+    
+    df['comment'] = df["comment"].apply(ggtranslation_delete)
+    df.comment = df["comment"].fillna('')
+    
+    df = df.merge(df_stations,
+                            left_on='location',
+                            right_on='name',
+                            how='left')
+
+    df = df.rename(columns={'reviewId':'id', 'storeCode':'STATIONID', 'comment' : 'text','Location Name':'STORENAME'})
+    df = df[['id','USERID','SOURCE','STATIONID','SURVEYDATE','REFUELDATE','RESPONSERECOMMANDATION','text','USERNAME','Country','Country name','REGION','MANAGEMENT MODE','STORENAME']]
+    return df
+
